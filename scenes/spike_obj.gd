b1f37d751f23a9ca8f3edb0d408314cb20fd32a4
Olaoluwa Dayo-Olaide (pla326) <pla326@mail.usask.ca>
2025-05-31 15:01:23 -0600

    built level one two and three using the tiles, added collision objects using animation player and a couple other nodes, collision objects so far include the swinging object and the surpise spikes, everything works fine, need to implement moving platforms and tweens

---
+++ scenes/spike_obj.gd
@@ -0,0 +1,13 @@
+extends Area2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+func _physics_process(delta: float) -> void:
+	$AnimationPlayer.play("Spikeout")
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		print("collision detected")
--- scripts/templayer.gd
+++ scripts/templayer.gd
@@ -1,9 +1,11 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const SPEED = 250.0
+const JUMP_VELOCITY = -350.0
 
+func _ready() -> void:
+	add_to_group('player')
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -11,7 +13,7 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("ui_up") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
